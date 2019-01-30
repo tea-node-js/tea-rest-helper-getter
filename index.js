3344eb557e23a72823d658f741ded4d7eff66636
baiyu <baiyu@admaster.com.cn>
2019-01-30 11:40:41 +0800

    move back

---
+++ index.js
@@ -0,0 +1,49 @@
+const delegate = require('func-delegate');
+const _ = require('lodash');
+
+const modelInclude = (params, includes) => {
+  if (!includes) return undefined;
+  if (!_.isString(params.includes)) return undefined;
+  const ret = _.filter(params.includes.split(','), x => includes[x]);
+  if (ret.length === 0) return undefined;
+  return _.map(ret, x => _.clone(includes[x]));
+};
+
+const getter = (Model, hook, keyPath) => (
+  async (ctx, next) => {
+    const id = _.get(ctx, keyPath);
+    const include = modelInclude(ctx.params, Model.includes);
+    const opt = { where: { id } };
+    if (include) opt.include = include;
+    try {
+      const model = await Model.find(opt);
+      ctx.hooks[hook] = model;
+      await next();
+    } catch (error) {
+      ctx.res.sequelizeIfError(error);
+    }
+  }
+);
+
+module.exports = (rest) => {
+  const { Sequelize } = rest;
+
+  rest.helper.getter = delegate(getter, [{
+    name: 'Model',
+    type: Sequelize.Model,
+    message: 'Model must be a class of Sequelize defined',
+  }, {
+    name: 'hook',
+    type: String,
+    allowNull: false,
+    message: 'Geted instance will hook on req.hooks[hook], so `hook` must be a string',
+  }, {
+    name: 'keyPath',
+    type: String,
+    allowNull: false,
+    defaultValue: 'params.id',
+    message: 'Gets the value at path of object.',
+  }]);
+
+  return rest.helper.getter;
+};
---
+++ test/getter.js
@@ -0,0 +1,271 @@
+
+const assert = require('assert');
+const rest = require('tea-rest');
+const om = require('tea-rest-plugin-mysql');
+const getterHelper = require('../');
+
+om(rest);
+
+const getter = getterHelper(rest);
+const { Sequelize } = rest;
+const sequelize = new Sequelize();
+
+const Book = sequelize.define('book', {
+  id: {
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: Sequelize.STRING,
+});
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: Sequelize.STRING,
+});
+
+/* global describe it */
+describe('tea-rest-helper-getter', () => {
+  describe('Helper init', () => {
+    it('First argument type error', (done) => {
+      assert.throws(() => {
+        getter({});
+      }, err => err instanceof Error && err.message === 'Model must be a class of Sequelize defined');
+      done();
+    });
+
+    it('Second argument must be string', (done) => {
+      assert.throws(() => {
+        getter(Book);
+      }, (err) => {
+        const msg = 'Geted instance will hook on req.hooks[hook], so `hook` must be a string';
+        return err instanceof Error && err.message === msg;
+      });
+      done();
+    });
+
+    it('Third argument must be string', (done) => {
+      assert.throws(() => {
+        getter(Book, 'book', []);
+      }, err => err instanceof Error && err.message === 'Gets the value at path of object.');
+      done();
+    });
+
+    it('All arguments right no exception', (done) => {
+      const helper = getter(Book, 'book');
+      const ctx = {
+        hooks: {},
+        params: { id: 20 },
+      };
+      const book = { id: 20, name: 'JavaScript 高级程序设计' };
+
+      Book.find = (opts) => {
+        assert.deepEqual({ where: { id: 20 } }, opts);
+        return new Promise((resolve) => {
+          resolve(book);
+        });
+      };
+
+      helper(ctx, (err) => {
+        assert.equal(null, err);
+        assert.equal(ctx.hooks.book, book);
+        done();
+      });
+    });
+
+    it('All arguments right no exception id = req.hooks[obj][id]', (done) => {
+      const helper = getter(Book, 'book', 'hooks.user.bookId');
+      const ctx = {
+        hooks: {
+          user: { bookId: 30 },
+        },
+        params: { id: 20 },
+      };
+      const book = { id: 20, name: 'JavaScript 高级程序设计' };
+
+      Book.find = (opts) => {
+        assert.deepEqual({ where: { id: 30 } }, opts);
+        return new Promise((resolve) => {
+          resolve(book);
+        });
+      };
+
+      helper(ctx, (err) => {
+        assert.equal(null, err);
+        assert.equal(ctx.hooks.book, book);
+        done();
+      });
+    });
+
+    it('All arguments right has exception', (done) => {
+      const helper = getter(Book, 'book');
+      const ctx = {
+        hooks: {},
+        params: { id: 20 },
+        res: {
+          sequelizeIfError: ({
+            errors = null, message = null,
+          }) => {
+            assert.equal(null, errors);
+            assert.equal('Find book error', message);
+            done();
+          },
+        },
+      };
+
+      const error = Error('Find book error');
+
+      Book.find = (opts) => {
+        assert.deepEqual({ where: { id: 20 } }, opts);
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      };
+
+      helper(ctx);
+    });
+
+    it('All arguments right includes', (done) => {
+      Book.includes = {
+        user: {
+          model: User,
+          as: 'creator',
+          required: true,
+        },
+      };
+
+      const helper = getter(Book, 'book');
+      const ctx = {
+        hooks: {},
+        params: { id: 20, includes: 'user' },
+        res: {
+          sequelizeIfError: ({
+            errors = null, message = null,
+          }) => {
+            assert.equal(null, errors);
+            assert.equal('Find book error', message);
+            done();
+          },
+        },
+      };
+
+      const error = Error('Find book error');
+
+      Book.find = (opts) => {
+        assert.deepEqual({
+          where: { id: 20 },
+          include: [{
+            model: User,
+            as: 'creator',
+            required: true,
+          }],
+        }, opts);
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      };
+
+      helper(ctx);
+    });
+
+    it('All arguments none includes', (done) => {
+      Book.includes = undefined;
+      const helper = getter(Book, 'book');
+      const ctx = {
+        hooks: {},
+        params: { id: 20, includes: 'user' },
+        res: {
+          sequelizeIfError: ({
+            errors = null, message = null,
+          }) => {
+            assert.equal(null, errors);
+            assert.equal('Find book error', message);
+            done();
+          },
+        },
+      };
+      const error = Error('Find book error');
+
+      Book.find = (opts) => {
+        assert.deepEqual({ where: { id: 20 } }, opts);
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      };
+
+      helper(ctx);
+    });
+    it('All arguments params includes isnt string', (done) => {
+      Book.includes = {
+        user: {
+          model: User,
+          as: 'creator',
+          required: true,
+        },
+      };
+
+      const helper = getter(Book, 'book');
+      const ctx = {
+        hooks: {},
+        params: { id: 20, includes: ['user'] },
+        res: {
+          sequelizeIfError: ({
+            errors = null, message = null,
+          }) => {
+            assert.equal(null, errors);
+            assert.equal('Find book error', message);
+            done();
+          },
+        },
+      };
+      const error = Error('Find book error');
+
+      Book.find = (opts) => {
+        assert.deepEqual({ where: { id: 20 } }, opts);
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      };
+
+      helper(ctx);
+    });
+    it('All arguments params includes dont match', (done) => {
+      Book.includes = {
+        user: {
+          model: User,
+          as: 'creator',
+          required: true,
+        },
+      };
+
+      const helper = getter(Book, 'book');
+      const ctx = {
+        hooks: {},
+        params: { id: 20, includes: 'author' },
+        res: {
+          sequelizeIfError: ({
+            errors = null, message = null,
+          }) => {
+            assert.equal(null, errors);
+            assert.equal('Find book error', message);
+            done();
+          },
+        },
+      };
+      const error = Error('Find book error');
+
+      Book.find = (opts) => {
+        assert.deepEqual({ where: { id: 20 } }, opts);
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      };
+
+      helper(ctx);
+    });
+  });
+});
